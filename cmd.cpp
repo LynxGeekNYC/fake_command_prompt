732d09e0097d80c2b7438bcb45155bcecaa84838
Alexander Mirvis <45396441+LynxGeekNYC@users.noreply.github.com>
2020-07-07 17:05:14 -0400

    Create cmd.cpp

---
+++ cmd.cpp
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+                             MS Command Prompt Emulator
+                                        By
+                                  Alexander Mirvis
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Microsoft Windows\n(c) 2019 Microsoft Corporation\n\n";
+    int i;
+    cout << "C:\\Windows\\Windows32\\";
+    cin >> i;
+    cout << "Text goes here";
+  return 0;
+
+    return 0;
+}
